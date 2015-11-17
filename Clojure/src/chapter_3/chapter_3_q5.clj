d6b567536aae624239547722533c29aa3dbdaf10
Leeor Engel <leeorengel@gmail.com>
2015-11-16 21:39:38 -0500

    Q5 re-worked

--- Clojure/src/chapter_3/chapter_3_q5.clj
+++ Clojure/src/chapter_3/chapter_3_q5.clj
@@ -2,53 +2,17 @@
   (:require [data-structures.stack :refer :all])
   (:import (data_structures.stack SimpleStack)))
 
-;; Algorithm: O(n^2)
-;; find minimum element, the sort is done when the minimum element is on the top of initial stack
-;; 1. perform compare-and-swap based on having min as top, then pop that min off and put on temp stack
-;; 2. keep going until you have one element left in the stack (this is the max element)
-;; 3. now perform the opposite operation on the temp stack using max element
-;; 4. if the initial stack has the minimum element at top then stop
-
-(defn- compare-and-swap
-  ([^SimpleStack stack cmp-fn]
-   (compare-and-swap stack (stack-pop stack) cmp-fn))
-  ([^SimpleStack stack x cmp-fn]
-   "Adds x, and compares x to stack's top and performs a swap such that top is in the result of applying the comparison function
-   cmp-fn"
-   (if (zero? (stack-size stack))
-     (stack-push stack x)
-     (if (cmp-fn (stack-peek stack) x)
-       (let [old-top-data (stack-pop stack)]
-         (stack-push stack x)
-         (stack-push stack old-top-data))
-       (stack-push stack x)))))
-
-(defn- swap-pass [^SimpleStack s1 ^SimpleStack s2 cmp-fn]
-  "Performs a compare-and-swap pass on s1 using cmp-fn and returns the min element based on comp-fn"
-  (loop [min nil]
-    (if (stack-empty? s1)
-      min
-      (do (compare-and-swap s1 cmp-fn)
-          (stack-push s2 (stack-pop s1))
-          (let [new-min (if (nil? min)
-                          (stack-peek s2)
-                          (if (<= min (stack-peek s2))
-                            min
-                            (stack-peek s2)))]
-            (compare-and-swap s2 (complement cmp-fn))
-            (recur new-min))))))
+(defn- insert-in-sorted-position [x ^SimpleStack s ^SimpleStack t]
+  (while (and (not (stack-empty? t)) (> x (stack-peek t)))
+    (stack-push s (stack-pop t)))
+  (stack-push t x))
 
 (defn sort-stack [^SimpleStack stack]
-  (let [temp (create-stack)]
-    (loop [min nil
-           s stack
-           t temp
-           cmp-fn <]
-      (if (and (some? min) (pos? (stack-size stack)) (= min (stack-peek stack)))
-        stack
-        (let [new-min (swap-pass s t cmp-fn)]
-          (recur
-            (if (nil? min) new-min min)
-            t
-            s
-            (partial (complement cmp-fn))))))))
+  (if (>= 1 (stack-size stack))
+    stack
+    (let [temp (create-stack)]
+      (loop [current (stack-pop stack)]
+        (insert-in-sorted-position current stack temp)
+        (if (stack-empty? stack)
+          temp
+          (recur (stack-pop stack)))))))
--- Clojure/test/chapter_3/chapter_3_q5_test.clj
+++ Clojure/test/chapter_3/chapter_3_q5_test.clj
@@ -30,6 +30,26 @@
         (is (= 3 (stack-pop sorted)))
         (is (= 4 (stack-pop sorted))))))
 
+  (testing "Reverse order"
+    (let [stack (create-stack 1)]
+      (stack-push stack 2)
+      (stack-push stack 3)
+      (stack-push stack 4)
+      (stack-push stack 5)
+      (stack-push stack 6)
+      (stack-push stack 7)
+      (stack-push stack 8)
+
+      (let [sorted (sort-stack stack)]
+        (is (= 1 (stack-pop sorted)))
+        (is (= 2 (stack-pop sorted)))
+        (is (= 3 (stack-pop sorted)))
+        (is (= 4 (stack-pop sorted)))
+        (is (= 5 (stack-pop sorted)))
+        (is (= 6 (stack-pop sorted)))
+        (is (= 7 (stack-pop sorted)))
+        (is (= 8 (stack-pop sorted))))))
+
   (testing "Basic sort"
     (let [stack (create-stack 6)]
       (stack-push stack 34)
